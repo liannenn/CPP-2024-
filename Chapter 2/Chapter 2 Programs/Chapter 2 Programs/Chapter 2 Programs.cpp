44699abe8fe09f187fc75b750d17662a0dfd1a35
liannenn <hiraethals@gmail.com>
2024-08-21 14:50:53 -0500

    caroline

--- Chapter 2/Chapter 2 Programs/Chapter 2 Programs/Chapter 2 Programs.cpp
+++ Chapter 2/Chapter 2 Programs/Chapter 2 Programs/Chapter 2 Programs.cpp
@@ -1,20 +1,11 @@
-// Chapter 2 Programs.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Chapter 2 Programs
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	//compute total sales tax on a 75, 85, and 100 dollar purchase
+	//state sales tax is 4% and county is 2%
+
+	double 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
