d6e7f1e8a85471a39ce94c4b61d9b528f2b63b3f
liannenn <hiraethals@gmail.com>
2024-08-27 14:49:48 -0500

    ee

--- Chapter 3 Example Programs/Chapter 3 Example Programs/Chapter 3 Example Programs.cpp
+++ Chapter 3 Example Programs/Chapter 3 Example Programs/Chapter 3 Example Programs.cpp
@@ -33,6 +33,8 @@ int main()
 	//print the area
 	cout << "The area of a rectangle with a length of " << length << " and width of " << width << " is " << area << endl;
 
+	cout << "\n\n---------------------------------------------------------------------------------------------------\n\n";
+
 /*Example Program 3-1
   Accepts no arguments
   It takes input from the user for three values, first an integer, then a double, then a character. 
@@ -49,6 +51,8 @@ int main()
 		cout << "For the double value you entered: " << double_value << endl;
 		cout << "FOr the character you entered: " << letter << endl;
 
+		cout << "\n\n---------------------------------------------------------------------------------------------------\n\n";
+
 /*Example Program 3-4
   Accepts no arguments
   Takes input from the user for the length of one side of a square and finds the area of the square using the pow() function
@@ -63,9 +67,123 @@ int main()
 	cin >> side_of_square; 
 
 	//calculate the area
-	area_of_square = pow(side_of_square);
+	area_of_square = pow(side_of_square, 2);
 
 		//print the area 
-	cout << "The area of a square with a side measurement of " << side_of_square << " is: " << area_of_square;
+	cout << "\nThe area of a square with a side measurement of " << side_of_square << " is: " << area_of_square << endl << endl;
 
+	cout << "\n\n---------------------------------------------------------------------------------------------------\n\n";
+
+//Example Program 3-5
+	// Accepts no arguments
+	// Takes inputs from theuser for the radius of a circle,
+	// calculates the area using the formula area = PI * R^2
+	// and outputs it to the user 
+	
+	// intialize a constant for pi with the value 3.14159
+	const double pi = 3.14159;
+	double radius, area_circle;
+
+	//ask for radius
+	cout << "Enter a value for the radius of a circle: ";
+	//store the number
+	cin >> radius;
+
+	//calculate the area
+	area_circle = pow(radius, 2) * pi;
+
+		//print the area
+		cout << "\nThe area of a circle wth the radius " << radius << " is " << area_circle << endl;
+
+		cout << "\n\n---------------------------------------------------------------------------------------------------\n\n";
+
+//Example Program 3-6
+		//Accepts no arguments
+		//Takes input from the user for three test scores and finds and outputs the average
+
+		//initiate variable for three people
+		const double three_scores = 3;
+		//initiate a variable to store the three scores
+		double score_1, score_2, score_3, average_score;
+
+		//print asking for score 1
+		cout << "Input the score for test #1: ";
+		cin >> score_1;
+
+		//print asking for score 2
+		cout << "Input the score for test #2: ";
+		cin >> score_2;
+
+		//print asking for score 3
+		cout << "Input the score for test #3: ";
+		cin >> score_3;
+
+		//calculate for the average score
+		average_score = (score_1 + score_2 + score_3) / three_scores;
+
+		//print the average scores
+		cout << "\nThe average of the three test scores " << score_1 << ", " << score_2 << ", and " << score_3 << " is: " << average_score;
+
+		cout << "\n\n---------------------------------------------------------------------------------------------------\n\n";
+
+//Example Program 3-7
+		//Accepts no arguments
+		// Demonstrates overflow and unferflow of an integer
+
+		//initalize testVar ( max val for a short)
+		short testVar = 32767;
+
+		//output testVar
+		cout << "Maximum short integer value: " << testVar << endl;
+
+		//add 1 to make it overflow and output it
+		testVar = testVar + 1;
+		cout << "\nAdd 1 to make testVar overflow:" << testVar << endl;
+
+		//subtract 1 to make it underflow
+		testVar = testVar - 1;
+		cout << "Subtract 1 to make testVar underflow back to it's original value: " << testVar << endl; 
+
+		cout << "\n\n---------------------------------------------------------------------------------------------------\n\n";
+
+//Example Program 3-8
+		//accepts no arguments
+		//demonstrates overflow and unferflow with max float being 2.0e38
+
+		//define variables
+		float test;
+
+		//multiply to overflow the float test and output results
+		test = 2.0e38 * 1000;
+		cout << "2.0e38 * 1000 results in the following overflow: " << test << endl;
+
+		//divide to the underflow test and output the results
+		test = 2.0e-38 / 2.0e38;
+		cout << "2.0e-38 / 2.0e38 results in the following underflow: " << test << endl;
+
+		cout << "\n\n---------------------------------------------------------------------------------------------------\n\n";
+
+//Example Program 3-9
+		//accepts no arguments
+		//takes input from the user for the number of books read and the number of months to read them
+		//make both integers
+		//calculate the average number of books to read to meet the book goal. 
+		//use type casting to prevent integer division
+
+		//define variables
+		int books, months;
+		double perMonth;
+
+		//print asking for number of books 
+		cout << "How many books do you plan to read? ";
+		cin >> books;
+
+		//print asking for months
+		cout << "\nHow many months will it take you to read them? ";
+
+		//calculate the amount of books read per month
+		perMonth = static_cast<double>(books) / months;
+
+		//print amount read per month
+		cout << "You will need to read " << perMonth << " books per month.";
 }
