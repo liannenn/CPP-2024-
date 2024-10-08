87d0d5f62d42fbd1c911baec17dc800fa2e8beea
liannenn <hiraethals@gmail.com>
2024-10-08 14:04:51 -0500

    10/8/2024

--- 5-15 Edxample (Writing to a File)/5-15 Edxample (Writing to a File)/5-15 Edxample (Writing to a File).cpp
+++ 5-15 Edxample (Writing to a File)/5-15 Edxample (Writing to a File)/5-15 Edxample (Writing to a File).cpp
@@ -10,6 +10,7 @@ using namespace std;
 
 int main()
 {
+	string friend_1, friend_2, friend_3;
 	//Program 5-15
 	//Accepts no arguments
 	//Opens a file named namesdemo.txt to write in 
@@ -17,10 +18,8 @@ int main()
 	//Afterwards close the file
 
 	//create file and open it
-	ofstream outFile;
+	ofstream outFile("C:\\Users\\2017696\\Desktop\\CPP-2024\\datafiles\\namesdemo.txt");
 
-	//opens the file 
-	outFile.open("namesdemo.txt");
 
 	if (!outFile)
 		cout << "File not found.";
@@ -41,6 +40,32 @@ int main()
 
 	//close the file and output a message 
 		outFile.close();
-		cout << "All names sucessfully written to file: namesdemon.txt";
+		cout << "All names sucessfully written to file: namesdemo.txt";
 	}
+
+
+	//create file and open it
+	ofstream outFile("C:\\Users\\2017696\\Desktop\\CPP-2024\\datafiles\\demofriends.txt");
+
+	if (!outFile)
+		cout << "File demofriends.txt not found.";
+	else
+	{
+		cout << "Enter your first friend's name: ";
+		cin >> friend_1;
+
+		cout << "Enter the second friend's name: ";
+		cin >> friend_2;
+
+		cout << "Enter the third friend's name: ";
+		cin >> friend_3;
+
+		outFile << friend_1 << endl << friend_2 << endl << friend_3 << endl;
+
+		outFile.close();
+		cout << "All of your friend's names have been sucessfully written to the file: demofriends.txt.";
+	}
+
+
+
 }
