2e31653759426523426fc5a08fd55e886b4b776b
liannenn <hiraethals@gmail.com>
2025-01-15 14:47:54 -0600

    e3

--- DisplayMenu/DisplayMenu/Exercise2.cpp
+++ DisplayMenu/DisplayMenu/Exercise2.cpp
@@ -12,6 +12,22 @@ using namespace std;
 
 int Exercise2()
 {
-	"C:\Users\2017696\Desktop\CPP-2024\TeamNames.txt"
-	"C:\Users\2017696\Desktop\CPP-2024\WorldSeriesWinners.txt"
+	string team_names; //initializes variable to read TeamNames
+	double amt_of_teams = 29;
+	//words in each name alphabetically
+	int letters_per_name[28] = { 2, 2, 2, 2, 2, 3, 2, 2, 3, 2, 2, 2, 2, 3, 3, 2, 2, 3, 3, 3, 2, 2, 2, 2, 3, 3, 3, 3, 2 };
+
+	//makes TeamNames 
+	ofstream TeamNames("C:\Users\2017696\Desktop\CPP-2024\TeamNames.txt");
+	ofstream WorldSeriesWinners("C:\Users\2017696\Desktop\CPP-2024\WorldSeriesWinners.txt");
+
+	cout << "The following teams have won the World Series at least once: \n";
+
+	for (i=0; i<=amt_of_teams; i++)
+	{cout << 
+	}
+
+	TeamNames.close();
+	WorldSeriesWinners.close();
+
 }
